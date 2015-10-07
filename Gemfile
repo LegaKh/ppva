1da2b1b8d86f884642bec2c133873d81e1c9e9db
Oleg Perevertailo <Legakh@gmail.com>
2015-10-07 22:58:07 +0300

    sound alert

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
-source 'https://rubygems.org'
-
 gem 'watir-webdriver'
 gem 'gmail'
 gem 'telegram-bot-ruby'
--- lib/notifyer.rb
+++ lib/notifyer.rb
@@ -1,8 +1,9 @@
 module Notifyer
-	def self.notify(message = 'Hurry Up!')
+  def self.notify(message = 'Hurry Up!')
+    fork { exec 'mpg123','-q', 'alert.mp3' }
     threads = []
     threads << Thread.new { TelegramNotifier.notify(message) } if TELEGRAM_TOKEN.length > 0
     threads << Thread.new { NotificationMailer.notify(message) } if G_LOGIN.length > 0
     threads.each(&:join)
-	end
+  end
 end
